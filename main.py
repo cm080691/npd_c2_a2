a94324e83646bfb2398135a4af8885122e3c75b3
cm080691 <more08.chetan@gmail.com>
2016-07-14 22:12:10 +0000

    AS2 a2_shapes_classes

--- main.py
+++ main.py
@@ -21,6 +21,37 @@ add_enemy
 
 '''
 
+class Triangle(object):
+    shape_type = 'triangle'
+
+    def __init__(self, edge_length, name, allies, enemies):
+        self.edge_length = edge_length
+        self.name = name
+        self.allies = allies
+        self.enemies = enemies
+
+
+    def area(self):
+        return self.edge_length * self.edge_length * 0.5
+
+    def perimeter(self):
+        return self.edge_length + self.edge_length + self.edge_length
+
+    def update_edge_length(self, new_length):
+        self.edge_length = new_length
+
+    def add_ally(self, self_object):
+        self.alies.append(shape_object)
+
+    def add_enemy(self, shape_object):
+        self.enemies.append(shape_object)
+
+if __name__ == '__main__':
+    triangle_marty = Triangle(5, "marty", [], [])
+    print(triangle_marty.area())
+    print(triangle_marty.update_edge_length(10))
+    print(triangle_marty.area())
+
 class Square(object):
     shape_type = 'square'
 
@@ -50,3 +81,35 @@ if __name__ == '__main__':
     print(square_marty.area())
     print(square_marty.update_edge_length(10))
     print(square_marty.area())
+
+class Circle(object):
+    shape_type = 'circle'
+    PI = 3.14159
+
+    def __init__(self, edge_length, name, allies, enemies):
+        self.edge_length = edge_length
+        self.name = name
+        self.allies = allies
+        self.enemies = enemies
+
+
+    def area(self):
+        return self.edge_length * self.edge_length * self.PI
+
+    def perimeter(self):
+        return self.edge_length * 2 * self.PI
+
+    def update_edge_length(self, new_length):
+        self.edge_length = new_length
+
+    def add_ally(self, self_object):
+        self.alies.append(shape_object)
+
+    def add_enemy(self, shape_object):
+        self.enemies.append(shape_object)
+
+if __name__ == '__main__':
+    circle_marty = Circle(5, "marty", [], [])
+    print(circle_marty.area())
+    print(circle_marty.update_edge_length(10))
+    print(circle_marty.area())
